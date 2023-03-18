5ad11eb490e1d9b992b08e0cc05bf9c8565db26d
alan <alanprotesto@gmail.com>
2023-03-18 00:52:31 -0300

    formulario configurado

--- src/app/components/form-cadastro/form-cadastro.component.ts
+++ src/app/components/form-cadastro/form-cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-cadastro',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormCadastroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
+  formCadastro!: FormGroup;
   ngOnInit(): void {
+    this.createForm();
+  }
+
+
+  createForm() {
+    this.formCadastro = this.fb.group({
+      idCliente: [0],
+      data: [new Date().toLocaleDateString("pt-br"), Validators.required],
+      nome: ["", Validators.required],
+      telefone: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      cep: ["", [Validators.required]]
+
+    })
+
+  }
+
+  getErros(campo:string){
+    return this.formCadastro.get(campo)?.errors && this.formCadastro.get(campo)?.touched ? true:false
+  }
+
+  onSubmit() {
+    if (this.formCadastro.valid) {
+      alert(JSON.stringify(this.formCadastro.value, null, 2))
+    }else{
+      Swal.fire({
+        title:"Atenção",
+        icon:"info",
+        text:"por favor preencha todos os campos",
+        footer:new Date().toLocaleDateString()
+      })
+    }
   }
 
 }
